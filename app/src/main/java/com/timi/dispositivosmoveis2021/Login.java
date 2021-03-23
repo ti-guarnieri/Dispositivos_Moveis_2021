c3ce9c0d904e598b2ec2201b68f8b1c277960d7f
ti-guarnieri <ti_guarnieri@hotmail.com>
2021-03-23 13:36:36 -0300

    aula pronta!

--- app/src/main/java/com/timi/dispositivosmoveis2021/Login.java
+++ app/src/main/java/com/timi/dispositivosmoveis2021/Login.java
@@ -9,41 +9,37 @@ import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
-    private EditText edUser = null;
-    private EditText edSenha = null;
+    private EditText edUser ;
+    private EditText edSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Faça o Login");
         setContentView(R.layout.activity_login);
-        edUser = (EditText)findViewById(R.id.activity_login_user);
-        edSenha = (EditText)findViewById(R.id.activity_login_senha);
-
+        edUser = (EditText) findViewById(R.id.activity_login_user);
+       edSenha = (EditText) findViewById(R.id.activity_login_senha);
     }
 
     public void onClick1 (View view){
-        valida_user();
+        validaUser();
     }
 
-    public void valida_user (){
+    private void validaUser (){
         String user = edUser.getText().toString();
         String senha = edSenha.getText().toString();
 
-        if (user == null){
+       if (user.equals(senha)){
+           Intent conteudo = new Intent( this, Conteudo.class);
+           startActivity(conteudo);
+
+        } else if (user != senha){
+           edSenha.requestFocus();
             AlertDialog.Builder dlg = new AlertDialog.Builder(this);
-            dlg.setTitle("Aviso");
-            dlg.setMessage("Usuario deve ser preenchido!");
+           dlg.setTitle("Aviso");
+           dlg.setMessage("Usuario e senha devem ser iguais!");
             dlg.setNeutralButton("OK",null);
             dlg.show();
-        } else if (senha == null){
-            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
-            dlg.setTitle("Aviso");
-            dlg.setMessage("Senha deve ser preenchida!");
-            dlg.setNeutralButton("OK",null);
-            dlg.show();
-        } else if (user == senha){
-            Intent conteudo = new Intent( this, Conteudo.class);
-            startActivity(conteudo);
         }
     }
 }
